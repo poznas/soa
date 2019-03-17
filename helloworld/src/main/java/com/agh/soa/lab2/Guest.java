a82111dad73a51ff330336d3c4c0a0a6964c18d5
poznas <misterdannypl@gmail.com>
2019-03-17 21:17:35 +0100

    lab2 - guest board

---
+++ helloworld/src/main/java/com/agh/soa/lab2/Guest.java
@@ -0,0 +1,17 @@
+package com.agh.soa.lab2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Guest {
+
+    private String login;
+
+    private String email;
+}
